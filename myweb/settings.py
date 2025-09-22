21cb265c62d7e05c9cb0e8e249d491b6c3e91308
aajetechnology <aajetechnology@gmail.com>
2025-09-22 11:39:32 -0400

    Setup whitenoise for static files

--- myweb/settings.py
+++ myweb/settings.py
@@ -49,6 +49,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # add this
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    
+
 ]
 
 ROOT_URLCONF = 'myweb.urls'
@@ -125,6 +130,7 @@ STATICFILES_DIRS = [
 ]
 
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
  # for collectstatic in production
